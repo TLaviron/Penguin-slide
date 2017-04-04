c411ad2fddc92e705bf69273b32276db7c5ac6c6
Thierry Laviron <thierry.laviron@ensimag.grenoble-inp.fr>
2017-04-04 10:45:15 +0200

    ajout d'un générateur virtuel de relief

---
+++ src/TerrainGenerator.cpp
@@ -0,0 +1,20 @@
+/*
+ * TerrainGenerator.cpp
+ *
+ *  Created on: Apr 4, 2017
+ *      Author: ekhalyocthor
+ */
+
+#include "../include/TerrainGenerator.hpp"
+
+TerrainGenerator::TerrainGenerator(float variation) :
+        m_variation(variation) {
+}
+
+TerrainGenerator::~TerrainGenerator() {
+}
+
+float TerrainGenerator::getVariation() {
+    return m_variation;
+}
+
